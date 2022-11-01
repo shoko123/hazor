d11b2e847cbf746122cc670fbc752ce15a28a9bb
shoko gabby <shoko.gabby@gmail.com>
2022-10-31 19:00:55 -0500

    MediaSquare and overlays - basic functionality

--- resources/js/components/media/MediaSquare.vue
+++ resources/js/components/media/MediaSquare.vue
@@ -1,15 +1,25 @@
 <template>
-  <v-card class="mx-auto" max-width="size" max-height="size">
-    <v-img v-if="itemHasUrls" :src="item.urls.full" :lazy-src="item.urls.tn" contain aspect-ratio="1"
-      class="grey lighten-2">
-      <v-btn class="text-subtitle-1 font-weight-medium black--text" color="grey">{{ tagText }}</v-btn>
-      <v-card class="mx-auto" color="transparent" flat>
-        <v-card-text class="text-body-1 white--text">
-          {{ overlayText }}</v-card-text>
-      </v-card>
-    </v-img>
-  </v-card>
-
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card :class="{ 'on-hover': isHovering }" v-bind="props" :color="isHovering ? 'blue' : undefined">
+      <v-img v-if="itemHasUrls" :src="item.urls.full" :lazy-src="item.urls.tn" contain aspect-ratio="1"
+        class="grey lighten-2">
+        <v-btn class="text-subtitle-1 font-weight-medium black--text" color="grey">{{ tagText }}</v-btn>
+        <v-card class="mx-auto" color="transparent" flat>
+          <v-card-text class="text-body-1 white--text">
+            {{ overlayText }}</v-card-text>
+        </v-card>
+        <v-overlay v-if="isHovering">
+          <template #activator="{ isActive, props }">
+            <component
+                v-bind:is="overlay"
+                :item="item"
+                :source="source"
+              ></component>
+          </template>
+        </v-overlay>
+      </v-img>
+    </v-card>
+  </v-hover>
 </template>
 
 <script lang="ts" setup >
@@ -33,11 +43,18 @@ const props = defineProps<{
   size?: number
 }>()
 
+const myProps = computed(() => {
+  return props
+})
+
 //Runtime verification that we indeed have IMediaItem (and not IChipItem or ITableItem)
 const itemHasUrls = computed(() => {
   return typeof props.item.urls === 'object'
 })
 
+const show = computed(() => {
+  return true
+})
 const tagText = computed(() => {
   return props.item.tag
 })
@@ -47,7 +64,14 @@ const overlayText = computed(() => {
 })
 
 const overlay = computed(() => {
-  return props.item.tag
+  switch (props.source) {
+    case 'main':
+      return OverlayCollectionItem
+    case 'media':
+      return OverlayItemMedia
+    case 'related':
+      return OverlayRelated
+  }
 })
 
 </script>
--- resources/js/components/media/OverlayCollectionItem.vue
+++ resources/js/components/media/OverlayCollectionItem.vue
@@ -1,60 +1,49 @@
 <template>
   <v-card class="mx-auto" color="transparent" flat>
-    <v-card-text v-if="hasMedia" class="text-body-1 white--text"> {{ text }}</v-card-text> 
+    <v-card-text class="text-body-1 white--text"> {{text}}
+    </v-card-text>
     <v-card-actions>
-    <v-btn @click="goTo(item)">Visit</v-btn>
-    <v-btn v-if="hasMedia" @click="openLightBox()">Lightbox</v-btn>
-  </v-card-actions>
+      <v-btn @click="goTo(item)">Visit</v-btn>
+      <v-btn @click="openLightBox()">Lightbox</v-btn>
+    </v-card-actions>
   </v-card>
   <!--h5 v-if="hasMedia">{{ text }}</h5-->
- 
+
 </template>
     
 
-<script>
-export default {
-  props: {
-    item: Object,
-    page: Number,
-    index: Number,
-  },
+<script lang="ts" setup >
+import { TSource, IMediaItem } from '../../types/collectionTypes'
+import { computed, onMounted } from 'vue'
+const props = defineProps<{
+  source: TSource,
+  item: IMediaItem,
+  pageNoB1?: number,
+}>()
 
-  created() {
-    //console.log(`collectionOL page: ${this.page} index: ${this.index} item: ${JSON.stringify(this.item, null, 2)}`);
-  },
+onMounted(() => {
+  // text content should be the same as current `count.value`
+  console.log(`Overlay.onMounted props: ${JSON.stringify(props, null, 2)}`)
+})
+const text = computed(() => {
+  if (props.item.description === null) {
+    return "";
+  } else {
+    return props.item.description.length < 101 ? props.item.description : props.item.description.substr(0, 100) + "...";
+  }
+})
 
-  computed: {
-    hasMedia() {
-      return this.item.hasMedia;
-    },
-    text() {
-      let text = this.item.description;
-      if (text === null) {
-        return "";
-      } else {
-        return text.length < 101 ? text : text.substr(0, 100) + "...";
-      }
-    },
-  },
 
-  methods: {
-    openLightBox() {
-      let c = this.$store.getters["mgr/collections"]("main");
-      this.$store.commit("med/openLightBox", {
-        value: true,
-        source: "main",
-        index: this.index % c.itemsPerPage,
-      });
-    },
 
-    goTo(item) {
-      this.$store.dispatch("mgr/goToRoute", {
-        module: this.$store.getters["mgr/module"],
-        action: "show",
-        dot: item.dot
-      });
-    },
-  },
-};
+
+function openLightBox() {
+
+}
+
+function goTo(item: IMediaItem) {
+
+}
+
+
 </script>
 
